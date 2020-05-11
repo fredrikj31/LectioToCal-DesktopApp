ed2375eeb3d74f3969b70e60743d5da290559046
fredrikj31 <fredrik1206@gmail.com>
2020-05-11 13:29:02 +0200

    Fixed and removed useless function

--- assets/js/syncEvents.js
+++ assets/js/syncEvents.js
@@ -34,12 +34,19 @@ async function setupAuth() {
 }
 
 function authorize(callback) {
-	const oAuth2Client = new google.auth.OAuth2("294965687317-qca0crm8d3tu78nkkreuk73sdkdgsivh.apps.googleusercontent.com", "LdE_CuDQ6Mx0WLAJJOSEVOrz", "urn:ietf:wg:oauth:2.0:oob");
+	const oAuth2Client = new google.auth.OAuth2(
+		"294965687317-qca0crm8d3tu78nkkreuk73sdkdgsivh.apps.googleusercontent.com",
+		"LdE_CuDQ6Mx0WLAJJOSEVOrz",
+		"urn:ietf:wg:oauth:2.0:oob"
+	);
 
+	/* OLD CODE (NO NEED)
 	// Load client secrets from a local file.
-	const content = fs.readFileSync("./assets/data/credentials.json", "utf8");
-	const credentials = JSON.parse(content);
-	console.log(credentials)
+	// const content = fs.readFileSync("./assets/data/credentials.json", "utf8");
+	// const credentials = JSON.parse(content);
+	// console.log(credentials)
+	*/
+
 	// Check if we have previously stored a token.
 	fs.readFile(defaultLocation + "\\LectioToCal\\token.json", (err, token) => {
 		if (err) return getAccessToken(oAuth2Client, callback);
@@ -50,34 +57,31 @@ function authorize(callback) {
 
 function getEvents() {
 	authorize((auth) => {
-		listEventsFunc(auth);
-	});
-}
-
-function listEventsFunc(auth) {
-	const calendar = google.calendar({ version: "v3", auth });
-	calendar.events.list(
-		{
-			calendarId: "primary",
-			timeMin: new Date().toISOString(),
-			maxResults: 10,
-			singleEvents: true,
-			orderBy: "startTime",
-		},
-		(err, res) => {
-			if (err) return console.log("The API returned an error: " + err);
-			const events = res.data.items;
-			if (events.length) {
-				console.log("Upcoming 10 events:");
-				events.map((event, i) => {
-					const start = event.start.dateTime || event.start.date;
-					console.log(`${start} - ${event.summary}`);
-				});
-			} else {
-				console.log("No upcoming events found.");
+		const calendar = google.calendar({ version: "v3", auth });
+		calendar.events.list(
+			{
+				calendarId: "primary",
+				timeMin: new Date().toISOString(),
+				maxResults: 10,
+				singleEvents: true,
+				orderBy: "startTime",
+			},
+			(err, res) => {
+				if (err)
+					return console.log("The API returned an error: " + err);
+				const events = res.data.items;
+				if (events.length) {
+					console.log("Upcoming 10 events:");
+					events.map((event, i) => {
+						const start = event.start.dateTime || event.start.date;
+						console.log(`${start} - ${event.summary}`);
+					});
+				} else {
+					console.log("No upcoming events found.");
+				}
 			}
-		}
-	);
+		);
+	});
 }
 
 function stepper(number) {
